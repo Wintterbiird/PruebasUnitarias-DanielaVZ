fceff93f2f92b4e7157d1cbc8ad399d0cc12bff8
Dani <daniela.vilcazuniga@gmail.com>
2024-11-18 21:21:08 -0300

    todo listo

---
+++ prueba-entorno/src/views/AboutView.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="about">
+    <h1>Acerca de</h1>
+  </div>
+</template>
+
+<style>
+  h1{
+    text-align: center;
+  }
+</style>
+
+
---
+++ prueba-entorno/src/views/ContactView.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="contact">
+    <h1>Contacto</h1>
+  </div>
+</template>
+
+
+<style>
+  h1{
+    text-align: center;
+  }
+</style>
